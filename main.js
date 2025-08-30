a4351fb5401a251a5239e5242aafda85d8cfb5df
Karkkii <minecraft.karkkii@gmail.com>
2025-08-30 20:30:35 +0300

    Update main.js
    Uploaded functions: kysMessage, errorSupport, defaultTheme and changeTheme. Also removed "@echo error pupid" message in the console.

--- main.js
+++ main.js
@@ -6,9 +6,7 @@ console.warn("IF ANYONE WANTS YOU TO PASTE HERE SOMETHING, DONT DO IT!")
 console.warn("IF ANYONE WANTS YOU TO PASTE HERE SOMETHING, DONT DO IT!")
 console.warn("IF ANYONE WANTS YOU TO PASTE HERE SOMETHING, DONT DO IT!")
 console.warn("IF ANYONE WANTS YOU TO PASTE HERE SOMETHING, DONT DO IT!")
-console.warn("IF ANYONE WANTS YOU TO PASTE HERE SOMETHING, DONT DO IT!")
-console.warn("IF ANYONE WANTS YOU TO PASTE HERE SOMETHING, DONT DO IT!")
-console.log("If you have an error, please paste it like this: @echo error pupid!")
+console.warn("IF ANYONNYONE WANTS YOU TO PASTE HERE SOMETHING, DONT DO IT!")
 console.warn("IF ANYONE WANTS YOU TO PASTE HERE SOMETHING, DONT DO IT!")
 console.warn("IF ANYONE WANTS YOU TO PASTE HERE SOMETHING, DONT DO IT!")
 console.warn("IF ANYONE WANTS YOU TO PASTE HERE SOMETHING, DONT DO IT!")
@@ -18,3 +16,27 @@ console.warn("IF ANYONE WANTS YOU TO PASTE HERE SOMETHING, DONT DO IT!")
 console.warn("IF ANYONE WANTS YOU TO PASTE HERE SOMETHING, DONT DO IT!")
 console.warn("IF ANYONE WANTS YOU TO PASTE HERE SOMETHING, DONT DO IT!")
 console.warn("IF ANYONE WANTS YOU TO PASTE HERE SOMETHING, DONT DO IT!")
+
+function kysMessage() {
+  alert("Ayo, you are not alone! Please get help and don't hurt yourself!")
+  console.warn("This guy sent an message saying 'Kys'")
+  console.warn("Contacting support right now!")
+  errorSupport()
+}
+
+function errorSupport() {
+  console.error("An unexpected error occured while trying to contact support. Please try again later.")
+  alert("We are not contacting support about this!")
+}
+
+function defaultTheme() {
+  console.log("----------------------------")
+  console.log("System said: Dark theme")
+  console.log("----------------------------")
+}
+
+function changeTheme() {
+  console.log("----------------------------")
+  console.log("Changed theme to " + target.Theme)
+  console.log("----------------------------")
+}
